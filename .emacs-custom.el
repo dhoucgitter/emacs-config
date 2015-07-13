5336d22950652dc7e63f33072d6cc247f88b7050
David Houck <David.Houck@sas.com>
2015-07-13 14:04:02 -0400

    Add new file .emacs-custom

---
+++ .emacs-custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/org/taskdiary.org" "~/org/95deploy.org" "~/org/dailytasks.org" "~/org/pdttestteam.org")))
+ '(org-catch-invisible-edits (quote smart))
+ '(org-hide-emphasis-markers t)
+ '(org-log-done (quote time))
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t)" "INPROGRESS(p)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)"))))
+ '(org-use-speed-commands t)
+ '(recentf-mode t)
+ '(tool-bar-mode nil)
+ '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
